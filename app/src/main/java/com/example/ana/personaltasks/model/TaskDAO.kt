7129f9b15b870aaf73235530d065414772a01dc0
Turazzi <analivia.turazzi@gmail.com>
2025-05-21 14:48:17 -0300

    feat: Adds implemented TaskDAO interface

--- app/src/main/java/com/example/ana/personaltasks/model/TaskDAO.kt
+++ app/src/main/java/com/example/ana/personaltasks/model/TaskDAO.kt
@@ -1,4 +1,9 @@
 package com.example.ana.personaltasks.model
 
-class TaskDAO {
+interface TaskDAO {
+    fun createTask(task: Task): Long
+    fun retrieveTask(id: Int): Task
+    fun retrieveTasks(): MutableList<Task>
+    fun updateTask(task: Task): Int
+    fun deleteTask(id: Int): Int
 }

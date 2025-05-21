913a7425f99da60c927d07cfcd040526d751dbc3
Turazzi <analivia.turazzi@gmail.com>
2025-05-21 14:44:30 -0300

    feat: Adds constant class for task identifier

--- app/src/main/java/com/example/ana/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/ana/personaltasks/model/Task.kt
@@ -1,10 +1,12 @@
 package com.example.ana.personaltasks.model
 
 import android.os.Parcelable
+import com.example.ana.personaltasks.model.Constant.INVALID_TASK_ID
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Task(
+    var id: Int? = INVALID_TASK_ID,
     var titulo: String = " ",
     var descricao: String = " ",
     var dataLimite: String = ""

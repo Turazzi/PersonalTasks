0c895e255ae71f08ed83a1f33f87be084ecb5770
Turazzi <analivia.turazzi@gmail.com>
2025-05-22 12:50:47 -0300

    feat: Adds auxiliary layouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
+
 
 dependencies {
     implementation(libs.androidx.core.ktx)

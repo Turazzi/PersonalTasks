129e716cdd5d47dc18108bd2df87937e546418ed
Turazzi <analivia.turazzi@gmail.com>
2025-05-22 15:35:26 -0300

    feat: Adds implementation for MainController class

--- app/src/main/java/com/example/ana/personaltasks/controller/MainController.kt
+++ app/src/main/java/com/example/ana/personaltasks/controller/MainController.kt
@@ -1,4 +1,18 @@
 package com.example.ana.personaltasks.controller
 
-class MainController {
+import com.example.ana.personaltasks.model.Task
+import com.example.ana.personaltasks.model.TaskDAO
+import com.example.ana.personaltasks.model.TaskSqlite
+import com.example.ana.personaltasks.ui.MainActivity
+
+class MainController (mainActivity: MainActivity){
+
+    private val taskDAO: TaskDAO = TaskSqlite(mainActivity)
+
+    fun insertTask(task: Task) = taskDAO.createTask(task)
+    fun getTask(id: Int) = taskDAO.retrieveTask(id)
+    fun getTasks() = taskDAO.retrieveTasks()
+    fun modifyTask(task: Task) = taskDAO.updateTask(task)
+    fun removeTask(id: Int) = taskDAO.deleteTask(id)
+
 }

88be425dd298e07c9b5a9c9bad397770195f0648
Turazzi <analivia.turazzi@gmail.com>
2025-05-22 15:42:17 -0300

    feat: Starts classes necessary for the MainActivity to work

--- app/src/main/java/com/example/ana/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/example/ana/personaltasks/ui/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.example.ana.personaltasks.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ana.personaltasks.R
+import com.example.ana.personaltasks.adapter.TaskAdapter
+import com.example.ana.personaltasks.controller.MainController
+import com.example.ana.personaltasks.databinding.ActivityMainBinding
+import com.example.ana.personaltasks.model.Task
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : AppCompatActivity(), OnTaskClickListener {
+
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val taskList: MutableList<Task> = mutableListOf()
+
+    private val taskAdapter: TaskAdapter by lazy {
+        TaskAdapter(taskList, this)
+    }
+
+    private lateinit var acResult: ActivityResultLauncher<Intent>
+
+    private val mainController: MainController by lazy {
+        MainController(this)
+    }
+
+
+
 }

179eb6f51c6b3014ddfc5cacbce4a45d06865198
Turazzi <analivia.turazzi@gmail.com>
2025-05-22 15:57:29 -0300

    feat: Adds implementation for onCreate method in MainActivity

--- app/src/main/java/com/example/ana/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/example/ana/personaltasks/ui/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.ana.personaltasks.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ana.personaltasks.R
 import com.example.ana.personaltasks.adapter.TaskAdapter
 import com.example.ana.personaltasks.controller.MainController
 import com.example.ana.personaltasks.databinding.ActivityMainBinding
+import com.example.ana.personaltasks.model.Constant.EXTRA_TASK
 import com.example.ana.personaltasks.model.Task
 
 class MainActivity : AppCompatActivity(), OnTaskClickListener {
@@ -32,6 +37,63 @@ class MainActivity : AppCompatActivity(), OnTaskClickListener {
         MainController(this)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.task_list)
+
+        acResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == RESULT_OK) {
+
+                val task = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    result.data?.getParcelableExtra(EXTRA_TASK, Task::class.java)
+                }
+                else {
+                    result.data?.getParcelableExtra<Task>(EXTRA_TASK)
+                }
+
+                task?.let { receivedTask ->
+                    val position = taskList.indexOfFirst {it.id == receivedTask.id}
+                    if (position == -1) {
+                        taskList.add(receivedTask)
+                        taskAdapter.notifyItemChanged(position)
+                        mainController.insertTask(receivedTask)
+                    }
+                }
+            }
+        }
+
+        amb.taskRv.adapter = taskAdapter
+        amb.taskRv.layoutManager = LinearLayoutManager(this)
+
+        fillTaskList()
+
+    }
+
+    private fun fillTaskList() {
+
+        taskList.clear()
+        Thread {
+            taskList.addAll(mainController.getTasks())
+            taskAdapter.notifyDataSetChanged()
+        }.start()
+
+    }
+
+    override fun onTaskClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRemoveTaskMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEditTaskMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
+    }
 
 
 }

decf503edc8b9a91b0b7cc19970ba14bb9999977
Turazzi <analivia.turazzi@gmail.com>
2025-05-22 15:18:41 -0300

    feat: Adds TaskActivity implementation

--- app/src/main/java/com/example/ana/personaltasks/ui/TaskActivity.kt
+++ app/src/main/java/com/example/ana/personaltasks/ui/TaskActivity.kt
@@ -1,4 +1,84 @@
 package com.example.ana.personaltasks.ui
 
-class TaskActivity {
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ana.personaltasks.databinding.ActivityTaskBinding
+import com.example.ana.personaltasks.model.Constant.EXTRA_TASK
+import com.example.ana.personaltasks.model.Constant.EXTRA_VIEW_TASK
+import com.example.ana.personaltasks.model.Task
+
+class TaskActivity : AppCompatActivity() {
+
+    private val acb: ActivityTaskBinding by lazy {
+
+        ActivityTaskBinding.inflate(layoutInflater)
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(acb.root)
+
+        setSupportActionBar(acb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Nova task"
+
+        val receivedTask = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+
+            intent.getParcelableExtra(EXTRA_TASK, Task::class.java)
+
+        }
+        else {
+
+            intent.getParcelableExtra<Task>(EXTRA_TASK)
+
+        }
+
+        receivedTask?.let{
+
+            supportActionBar?.subtitle = "Editar"
+            with(acb) {
+                tituloEt.setText(it.titulo)
+                descricaoEt.setText(it.descricao)
+                dataEt.setText(it.dataLimite)
+
+                val viewTask = intent.getBooleanExtra(EXTRA_VIEW_TASK, false)
+                if(viewTask) {
+
+                    supportActionBar?.subtitle = "View task"
+                    tituloEt.isEnabled = false
+                    descricaoEt.isEnabled = false
+                    dataEt.isEnabled = false
+                    saveBt.visibility = View.GONE
+
+                }
+            }
+
+        }
+
+        with(acb) {
+
+            saveBt.setOnClickListener {
+                Task (
+                    receivedTask?.id?:hashCode(),
+                    tituloEt.text.toString(),
+                    descricaoEt.text.toString(),
+                    dataEt.text.toString()
+                ).let {
+                    task ->
+                        Intent().apply {
+                            putExtra(EXTRA_TASK, task)
+                            setResult(RESULT_OK, this)
+                        }
+                }
+                finish()
+            }
+
+        }
+
+    }
+
 }

6a1eadb9b942dedd3c8ad7a6c90d03d98fa62295
Turazzi <analivia.turazzi@gmail.com>
2025-05-22 21:32:03 -0300

    feat: Add comments in TaskDAO class

--- app/src/main/java/com/example/ana/personaltasks/model/TaskDAO.kt
+++ app/src/main/java/com/example/ana/personaltasks/model/TaskDAO.kt
@@ -1,5 +1,6 @@
 package com.example.ana.personaltasks.model
 
+// Interface que define as operações básicas para manipulação de tarefas no banco de dados
 interface TaskDAO {
     fun createTask(task: Task): Long
     fun retrieveTask(id: Int): Task

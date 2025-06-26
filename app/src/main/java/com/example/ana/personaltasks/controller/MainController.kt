051c6fea83aec7e07ff304763fe438feab2ea3c3
Turazzi <analivia.turazzi@gmail.com>
2025-06-26 01:30:09 -0300

    feat: Update MainController class

--- app/src/main/java/com/example/ana/personaltasks/controller/MainController.kt
+++ app/src/main/java/com/example/ana/personaltasks/controller/MainController.kt
@@ -1,20 +1,41 @@
 package com.example.ana.personaltasks.controller
 
+import androidx.room.RoomDatabase
 import com.example.ana.personaltasks.model.Task
 import com.example.ana.personaltasks.model.TaskDAO
-import com.example.ana.personaltasks.model.TaskSqlite
+import com.example.ana.personaltasks.model.TaskFirebase
 import com.example.ana.personaltasks.ui.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 // Controlador principal que serve como intermediário entre a interface (MainActivity) e o acesso a dados (DAO)
-class MainController (mainActivity: MainActivity){
+class MainController {
 
-    private val taskDAO: TaskDAO = TaskSqlite(mainActivity)
+    private val taskDAO: TaskDAO = TaskFirebase()
 
-    fun insertTask(task: Task) = taskDAO.createTask(task)
-    fun getTask(id: Int) = taskDAO.retrieveTask(id)
-    fun getTasks() = taskDAO.retrieveTasks()
-    fun modifyTask(task: Task) = taskDAO.updateTask(task)
-    fun removeTask(id: Int) = taskDAO.deleteTask(id)
-    fun findTasks(query: String) = taskDAO.searchTasks(query)
+    fun insertTask(task: Task) = CoroutineScope(Dispatchers.IO).launch {
+        taskDAO.createTask(task)
+    }
+
+    fun getTasks(callback: (List<Task>) -> Unit) {
+        taskDAO.retrieveTasks(callback)
+    }
+
+    fun getDeletedTasks(callback: (List<Task>) -> Unit) {
+        taskDAO.retrieveDeletedTasks(callback)
+    }
+
+    fun modifyTask(task: Task) = CoroutineScope(Dispatchers.IO).launch {
+        taskDAO.updateTask(task)
+    }
+
+    fun removeTask(task: Task) = CoroutineScope(Dispatchers.IO).launch {
+        taskDAO.deleteTask(task)
+    }
+
+    fun reactivateTask(task: Task) = CoroutineScope(Dispatchers.IO).launch {
+        taskDAO.reactivateTask(task)
+    }
 
 }
